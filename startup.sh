f76849341c63355224ae152c1dd9fb3ab0a0eb04
Sebastian Bachmann <me@free-minds.net>
2015-10-31 18:21:28 +0100

    changes for the desktop

--- startup.sh
+++ startup.sh
@@ -5,23 +5,11 @@ xset -dpms &
 xset s off &
 xset -b & 
 
-# screenlocker
-xautolock -detectsleep -time 3 -locker "i3lock -d -c 000070" -notify 30 -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'" &
-
-# mouse and trackball settings
-xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 8 1
-xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Button" 8 2
-xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Timeout" 8 200
-xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 6 7 4 5
-synclient TouchPadOff=1
-
 # keyboard layout
 setxkbmap us altgr-intl -option ctrl:nocaps
 
 # run a few things on startup
 wmname LG3D &
-wicd-gtk --tray &
-pidgin &
 
 xsetroot -solid "#111111"
 # feh --bg-scale ~/.i3/wallpaper/eye.jpg &
