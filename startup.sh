022b78694f136195b94b656d514b85d7f1ada6ac
Sebastian Bachmann <me@free-minds.net>
2016-04-02 11:57:17 +0200

    remove xrandr line

--- startup.sh
+++ startup.sh
@@ -12,8 +12,5 @@ setxkbmap us altgr-intl -option ctrl:nocaps
 wmname LG3D &
 pidgin &
 
-xsetroot -solid "#111111"
-feh --bg-scale ~/.i3/wallpaper/2001-a-space-odyssey-original_0.jpg &
 gpg-agent --daemon --write-env-file "${HOME}/.gpg-agent-info" &
 
-xrandr --output DVI-D-0 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate right --output DVI-I-1 --off --output DVI-I-0 --off --output DP-1 --primary --mode 2560x1440 --pos 1080x0 --rotate normal --output DP-0 --off
