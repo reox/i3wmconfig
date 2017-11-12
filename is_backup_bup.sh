022f32350174fffab455760697f52258b891f9f6
Sebastian Bachmann <me@free-minds.net>
2017-11-13 00:52:40 +0100

    adding bup script

---
+++ is_backup_bup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -f /var/run/user/1000/bup-backup.pid ]; then
+    # f085
+    echo ""
+    echo ""
+    echo "#cb4b16"
+else
+    echo ""
+    echo ""
+    echo "#859900"
+fi
+
+
+exit 0
