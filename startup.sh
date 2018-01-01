a44e9b38fcc606a972df07b6613cef2b83bccb32
Sebastian Bachmann <me@free-minds.net>
2018-01-01 17:43:03 +0100

    screensaver

--- startup.sh
+++ startup.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# disable dmps and bell
-xset -dpms &
-xset s off &
-xset -b &
+# disable dmps
+xset -dpms
+# Disable screensaver
+xset s off
+# Turn bell off
+xset b off
 
 # screenlocker
-xautolock -detectsleep -time 3 -locker "i3lock -i ~/.i3/lockscreen.png -f" -notify 30 -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'" &
+# xautolock -detectsleep -time 3 -locker "i3lock -i ~/.i3/lockscreen.png -f" -notify 30 -notifier "notify-send -u critical -t 10000 -- 'LOCKING screen in 30 seconds'" &
 
 # mouse and trackball settings
 xinput set-int-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation" 8 1
@@ -19,6 +21,5 @@ xinput set-prop "TPPS/2 IBM TrackPoint" "Evdev Wheel Emulation Axes" 6 7 4 5
 setxkbmap us altgr-intl -option ctrl:nocaps
 
 # run a few things on startup
-wmname LG3D &
-
+wmname LG3D
 gpg-agent --daemon --write-env-file "${HOME}/.gpg-agent-info"
