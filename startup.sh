b7176674437108a615c305d3007287ba3ca50f14
Sebastian Bachmann <me@free-minds.net>
2018-09-03 10:08:21 +0200

    startup

--- startup.sh
+++ startup.sh
@@ -3,10 +3,11 @@
 # disable dmps and bell
 xset -dpms &
 xset s off &
-xset -b & 
+xset -b &
 
 # keyboard layout
-setxkbmap us altgr-intl -option ctrl:nocaps
+# setxkbmap us altgr-intl -option ctrl:nocaps &
+# Should be set by /etc/default/keyboard
 
 # run a few things on startup
 wmname LG3D &
